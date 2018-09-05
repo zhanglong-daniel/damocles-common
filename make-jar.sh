7f58b864b23372e97bfa66c5bf4165c6d55ff4da
zhanglong02 <zhanglong02@baidu.com>
2018-09-05 15:51:38 +0800

    modify make-jar.sh

--- make-jar.sh
+++ make-jar.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 gradle clean
+
 gradle assembleRelease
-gradle makeJar
+
+rm -rf ./output/
+mkdir output
+cp ./library/build/intermediates/bundles/default/classes.jar ./output/
+time=`date +%Y%m%d%H%M%S`
+mv ./output/classes.jar ./output/damocles-common-$time.jar
+ls -l ./output/
